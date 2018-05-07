610387382ad0bb6038658b71a6adaf43356d4156
mexx <mexx.liang@gmail.com>
2018-05-07 10:26:03 +0800

    this is test

---
+++ app/src/main/java/com/example/oolong/login_test/Map.java
@@ -0,0 +1,13 @@
+package com.example.oolong.login_test;
+
+/**
+ * Created by PC-26 on 2018/5/7.
+ */
+
+public class Map {
+    //Github上傳測試用
+    //Github上傳測試用
+    //Github上傳測試用
+    //Github上傳測試用
+    //Github上傳測試用
+}
